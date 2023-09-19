5c52e78f77973839d77d367b297ee54a23e8b9c7
Umair <umairwasim001@gmail.com>
2023-09-20 01:48:59 +0500

    Update Card script
    Refine Card Initialization, Flip, and Click Action logic

--- NoTwins/Assets/Scripts/Card.cs
+++ NoTwins/Assets/Scripts/Card.cs
@@ -1,22 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] private CardSO cardSO;
-    [SerializeField] private Image cardImage;
+    public bool isFaceUp = false;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Sprite faceDownSprite;
+    [SerializeField] private Image cardImage;
+    [SerializeField] private Button cardButton;
+
+    private Action<Card> OnCardClick;
+    private Sprite faceUpSprite;
+
+    public void Initialize(Sprite faceUpSp, Action<Card> OnClick)
     {
-        
+        faceUpSprite = faceUpSp;
+        SetCardFaceUp();
+        cardButton.onClick.RemoveAllListeners();
+        cardButton.onClick.AddListener(OnButtonPressed);
+
+        OnCardClick = OnClick;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool HasSameSprite(Card card)
     {
-        
+        if (cardImage.sprite == card.cardImage.sprite)
+            return true;
+        return false;
+    }
+
+    private void OnButtonPressed()
+    {
+        OnCardClick.Invoke(this);
+    }
+
+    public void FlipCard()
+    {
+        if (isFaceUp)
+            SetCardFaceDown();
+        else
+            SetCardFaceUp();
+    }
+
+    public void SetCardFaceUp()
+    {
+        isFaceUp = true;
+        cardImage.sprite = faceUpSprite;
+    }
+
+    public void SetCardFaceDown()
+    {
+        isFaceUp = false;
+        cardImage.sprite = faceDownSprite;
+    }
+
+    public void SetCardMatched()
+    {
+        gameObject.SetActive(false);
     }
 }
+
